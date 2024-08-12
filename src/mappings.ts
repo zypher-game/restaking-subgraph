d53d587fbe6ddf6284bede7492c10ba9784f400d
Chris Chu <15345991+o-p@users.noreply.github.com>
2024-08-12 16:27:30 +0800

    Apply mainnet contracts

--- src/mappings.ts
+++ src/mappings.ts
@@ -2,16 +2,71 @@ import { BigInt } from '@graphprotocol/graph-ts';
 
 import {
   AnswerUpdated,
+  DataFeed as DataFeedContract
 } from '../generated/ChainlinkDataFeed/DataFeed'
-
-import { DataFeed as DataFeedContract } from '../generated/ChainlinkDataFeed/DataFeed'
+import {
+  Deposit as DepositEvent,
+  Withdraw as WithdrawEvent,
+} from '../generated/ZypherRestaking/Restaking'
 import { DataFeed } from '../generated/schema';
+import {
+  newDeposit,
+  newWithdraw,
+  touchUser,
+  touchToken,
+  increaseAsset,
+  decreaseAsset,
+} from './entities'
 
+/**
+ * @dev
+ *   event Deposit(
+ *     address indexed user,
+ *     address indexed token,
+ *     uint256 amount,
+ *     uint256 weekly
+ *   );
+ */
+export function handleDeposit(event: DepositEvent): void {
+  const user = touchUser(event.params.user)
+  const token = touchToken(event.params.token)
+  const amount = event.params.amount
+
+  newDeposit(
+    event.transaction.hash,
+    user,
+    token,
+    amount,
+    event.block.timestamp
+  )
+  increaseAsset(user, token, amount)
+}
+
+/**
+ * @dev
+ *   event Withdraw(
+ *     address indexed user,
+ *     address indexed token,
+ *     uint256 amount
+ *   );
+ */
+export function handleWithdraw(event: WithdrawEvent): void {
+  const user = touchUser(event.params.user)
+  const token = touchToken(event.params.token)
+  const amount = event.params.amount
+
+  newWithdraw(
+    event.transaction.hash,
+    user,
+    token,
+    amount,
+    event.block.timestamp
+  )
+  decreaseAsset(user, token, amount)
+}
+
+/** @dev params: current / roundId / updatedAt */
 export function handleAnswerUpdated(event: AnswerUpdated): void {
-  event.params.current;
-  event.params.roundId;
-  event.params.updatedAt;
-
   let feed = DataFeed.load(event.address)
   if (!feed) {
     const contract = DataFeedContract.bind(event.address)
